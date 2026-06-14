b3794a33415535125026b486b48cc05753a15089
Ovidiu <gg64nouit@gmail.com>
2026-06-14 12:45:05 +0300

    Update locales.ts

--- motomate/src/lib/i18n/locales.ts
+++ motomate/src/lib/i18n/locales.ts
@@ -5,8 +5,9 @@ import es from './locales/es.json';
 import it from './locales/it.json';
 import nl from './locales/nl.json';
 import pt from './locales/pt.json';
+import ro from './locales/ro.json';
 
-export const locales = { en, de, fr, es, it, nl, pt };
+export const locales = { en, de, fr, es, it, nl, pt, ro };
 export const supportedLocales = Object.keys(locales) as (keyof typeof locales)[];
 
 export function getLocale<T>(map: Record<string, T>, locale: string): T {
